3643de9fe091af6d47dcf48d27c5c14a9db8c830
sim0wastaken <104425606+sim0wastaken@users.noreply.github.com>
2025-04-17 12:43:14 +0200

    Implemented speed insights
    Grazie poiso

--- shouldigoskiing/src/app/layout.tsx
+++ shouldigoskiing/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
